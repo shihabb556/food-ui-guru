60f64e737a8ecdfca7502750201e63b4249002e6
shihabb556 <shihabaly556@gmail.com>
2024-09-28 00:43:11 +0600

    Update order.js

--- order.js
+++ order.js
@@ -21,7 +21,7 @@ document.getElementById('submitOrder').addEventListener('click', async (e) => {
 
   try {
     // Send POST request to the API
-    const response = await fetch('http://localhost:5000/api/orders', {
+    const response = await fetch('https://guru-food-server.vercel.app/api/orders', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
